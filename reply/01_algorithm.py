4f11fed74669d74e7f14234165edfb0a97748555
harimau99 <najoe.info@gmail.com>
2022-06-10 04:12:23 +0800

    add: folder name reply

---
+++ reply/01_algorithm.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# Using Leibnitz Formula 
+# https://www.cuemath.com/calculus/leibniz-rule/
+
+n = int(input("Enter an integer: "))
+k = 0
+pi = 0
+while k <= n:
+# the next one line is for the formula
+    ls = 4*((((-1)**k)/(2*k + 1.0)))
+    pi = pi + ls
+    print("%d: %.25f" %(k, pi))
+    k = k + 1
+
+# añadimos un comentario para comprensión de n_terms
+n_terms = n + 1
+
+if n == 0:
+    print("The result above, is the first value of pi calculated given by user.")
+else:
+    print("These are the first %d values of pi calculated given by user." %(n_terms))
+
